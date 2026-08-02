ef5b1b67d276e5a10790f941537c65e34176b48a
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-02 20:36:46 +0000

    Update MyContract.sol

--- fixtures/contracts/MyContract.sol
+++ fixtures/contracts/MyContract.sol
@@ -1,6 +1,4 @@
-
-        if (block.timestamp < lastUpdateAttempt + MIN_UPDATE_INTERVAL) {
-            revert UpdateTooFrequent(lastUpdateAttempt + MIN_UPDATE_INTERVAL);
+lastUpdateAttempt + MIN_UPDATE_INTERVAL);
         }
         _;
     }
