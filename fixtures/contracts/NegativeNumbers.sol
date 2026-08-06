b2784b1937631b2e169c0830bdf6b204c0edbcea
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-06 15:30:06 +0000

    Update NegativeNumbers.sol

--- fixtures/contracts/NegativeNumbers.sol
+++ fixtures/contracts/NegativeNumbers.sol
@@ -1,5 +1,5 @@
 =============================================
-    function updateRange(int256 ) external onlyOwner {
+    function updateRange(int256 ) exter
         if (newMin >= newMax) revert InvalidRange();
         if (_storedNumber < newMin || _storedNumber > newMax) revert OutOfRange();
 
