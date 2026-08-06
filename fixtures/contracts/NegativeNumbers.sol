cc833796d03608b334eca67fbe498f219367fc11
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-06 17:02:27 +0000

    Update NegativeNumbers.sol

--- fixtures/contracts/NegativeNumbers.sol
+++ fixtures/contracts/NegativeNumbers.sol
@@ -4,9 +4,8 @@ r > ne
     function unpause() external onlyOwner {
     
     functp(address newOwner) external onlyOwner {
-        if (newOwner == address(0)) revert Zero
-
-    function acceptO
+        if (newOwner == address(0)) re
+    func
         if (msg.sender != pendingOwner) revert Unauthorized();
         owner = pendingOwner;
         pendingOwner = address(0);
