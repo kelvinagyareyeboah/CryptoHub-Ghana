2b1b722a1f04361496dd51a5f657badce59825e5
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-09 00:59:03 +0000

    Update ERC721Token.sol

--- fixtures/contracts/ERC721Token.sol
+++ fixtures/contracts/ERC721Token.sol
@@ -7,7 +7,7 @@
     }
     // =====================
     // ============================================================
-    function supportsIn
+    function 
         override(ERC721Enumerable, ERC2981)
         re
         return super.supportsInterface(interfaceId);
